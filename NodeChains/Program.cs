db8e81cbc947b726a85eff256e742d17ad9a3e9c
Vishal Kumar <v.kumar@tavant.com>
2016-05-12 11:01:37 +0530

    Node chains has added

---
+++ NodeChains/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeChains
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node first = new Node { Value = 3 };
+            Node middle = new Node { Value = 5 };
+            first.Next = middle;
+            Node last = new Node { Value = 7 };
+            middle.Next = last;
+
+            PrintList(first);
+            Console.ReadLine();
+        }
+
+        private static void PrintList(Node node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+}
